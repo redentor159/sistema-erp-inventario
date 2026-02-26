8162a7d400a496599d839aa96b6bfc1af768cec0
JOSÉ SORIA <josesoria554@gmail.com>
2026-02-26 10:47:45 -0500

    chore: configure local webServer for playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,4 +18,9 @@ export default defineConfig({
             use: { ...devices['Desktop Chrome'] },
         },
     ],
+    webServer: {
+        command: 'npm run dev',
+        url: 'http://localhost:3000',
+        reuseExistingServer: !process.env.CI,
+    },
 });
